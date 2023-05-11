edb25d22f2f82258f8da176c2f08910890b262c1
Erika Duarte <erika.duarte.mora@gmail.com>
2023-05-10 22:09:19 -0500

    cambio logo y se arreglo input de busqueda

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,15 +7,15 @@ export default function Navbar() {
       <header>
         <nav className="nav-Navbar">
             <div className="logo">
-               <img src="https://img.icons8.com/metro/26/null/shop.png"/>
-               <h1>Store</h1> 
+            <img width="64" height="64" src="https://img.icons8.com/wired/64/000000/shop.png" alt="shop"/>
+                <h1>Store</h1> 
             </div>
             <div className="container-nav-list">
             <ul className="nav-list">
                 <li><a href="">Sign up</a></li>
                 <li><a href="">Login</a></li>
                 <li className="car">
-                  <button><img src="https://img.icons8.com/material/24/000000/add-shopping-cart--v1.png"/></button>  
+                  <button className="car-button"><img src="https://img.icons8.com/material/24/000000/add-shopping-cart--v1.png"/></button>  
                   <span>0</span>
                 </li>
             </ul>
